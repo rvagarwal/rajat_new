09469600f3702cf6a30c35ad285d3f04f022e0da
rajat.agarwal <rajatbansal96@gmail.com>
2018-06-13 15:56:43 +0530

    Added my_strcpy function

--- string.c
+++ string.c
@@ -6,15 +6,22 @@ while(*p)
 ++p;
 return(p-s);
 }
+char *my_strcpy(char *t,char *s)
+{
+char *p=t;
+while(*t++=*s++);
+return p;
+}
 int main(void)
 {
 int i;
-char *s[]=
-{
+char p1[32];
+char *s[]={
 "Git tutorials",
 "Tutorials Point"
 };
 for(i=0;i<2;++i)
 printf("string lenght of %s = %d\n",s[i],my_strlen(s[i]));
+printf("%s\n",my_strcpy(p1,"Hello, World !!!"));
 return0;
 }
