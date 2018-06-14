4b967894f29c7955af6527084fda95a770033ead
rajat.agarwal <rajatbansal96@gmail.com>
2018-06-14 11:37:30 +0530

    Patched string operations file

--- src/string_operations.c
+++ src/string_operations.c
@@ -34,6 +34,10 @@ char *u[]={
 "Git tutorials",
 "Tutorials Point"
 };
+char *u[]={
+"Git tutorials",
+"Tutorials Point"
+};
 for(i=0;i<2;++i)
 printf("string lenght of %s = %d\n",s[i],my_strlen(s[i]));
 printf("%s\n",my_strcpy(p1,"Hello, World !!!"));
